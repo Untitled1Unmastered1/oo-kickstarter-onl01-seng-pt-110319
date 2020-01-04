9c4bbc27b5d56dcedf2a04a73eaf7ae1d4848e9c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-04 03:25:40 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Backer 
+  attr_reader :backed_projects, :name 
+  attr_accessor :project
+  
+  
+  def initialize(name)
+    @name = name 
+    @backed_projects = []
+    @project = project 
+  end 
+  
+  def back_project(project)
+    @backed_projects << project 
+  end 
+end 
+    
